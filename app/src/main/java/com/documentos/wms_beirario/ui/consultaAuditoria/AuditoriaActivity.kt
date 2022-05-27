5d68abf7a728639c1b4ba9aa49c7331e9b1bf20d
deiviti <deivitifelski@outlook.com>
2022-05-27 14:36:31 -0300

    commit cmd

---
+++ app/src/main/java/com/documentos/wms_beirario/ui/consultaAuditoria/AuditoriaActivity.kt
@@ -0,0 +1,11 @@
+package com.documentos.wms_beirario.ui.consultaAuditoria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuditoriaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auditoria)
+    }
+}
