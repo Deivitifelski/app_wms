0b6e25ab69a0c72f32b56c137672832d4cedabb2
Deiviti Efisio <deiviti.efisio@paipe.co>
2023-04-03 15:37:32 -0300

    contador de registro etiquetagem por onda 1.0.52

--- app/src/main/java/com/documentos/wms_beirario/ui/boardingConference/BoardingconferenceActivity.kt
+++ app/src/main/java/com/documentos/wms_beirario/ui/boardingConference/BoardingconferenceActivity.kt
@@ -3,10 +3,15 @@ package com.documentos.wms_beirario.ui.boardingConference
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.documentos.wms_beirario.R
+import com.documentos.wms_beirario.databinding.ActivityBoardingconferenceBinding
 
 class BoardingconferenceActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityBoardingconferenceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_boardingconference)
+        mBinding = ActivityBoardingconferenceBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
     }
 }
