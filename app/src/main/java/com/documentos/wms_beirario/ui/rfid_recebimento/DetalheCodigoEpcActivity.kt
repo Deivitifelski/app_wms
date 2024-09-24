a3ad129aa3d19293084ef80e56350062572219e1
deiviti_efisio <deiviti.efisio@paipe.co>
2024-09-24 17:11:16 -0300

    init layout rfid v1

---
+++ app/src/main/java/com/documentos/wms_beirario/ui/rfid_recebimento/DetalheCodigoEpcActivity.kt
@@ -0,0 +1,19 @@
+package com.documentos.wms_beirario.ui.rfid_recebimento
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.documentos.wms_beirario.R
+import com.documentos.wms_beirario.databinding.ActivityDetalheCodigoEpcBinding
+
+class DetalheCodigoEpcActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetalheCodigoEpcBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetalheCodigoEpcBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
