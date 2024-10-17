2915995c43a11cbd9486908ea758d232cc11d0b8
deiviti_efisio <deiviti.efisio@paipe.co>
2024-10-17 10:16:20 -0300

    mensagem reimpressao por numero de serie ajustada

---
+++ app/src/main/java/com/documentos/wms_beirario/ui/rfid_recebimento/leituraEpc/adapter/LeituraRfidAdapter.kt
@@ -0,0 +1,66 @@
+package com.documentos.wms_beirario.ui.rfid_recebimento.leituraEpc.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.documentos.wms_beirario.R
+import com.documentos.wms_beirario.databinding.ItemRvEpcRfidBinding
+import com.documentos.wms_beirario.model.recebimentoRfid.LeituraRfidEpc
+import com.documentos.wms_beirario.model.recebimentoRfid.RecebimentoRfid
+
+
+class LeituraRfidAdapter(val onclick: (LeituraRfidEpc) -> Unit) :
+    RecyclerView.Adapter<LeituraRfidAdapter.LeituraRfidEpcAdapterRfidVh>() {
+
+    private var list = mutableListOf<LeituraRfidEpc>()
+
+    inner class LeituraRfidEpcAdapterRfidVh(val binding: ItemRvEpcRfidBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: LeituraRfidEpc) {
+            when (item.tipoLeitura) {
+                "E" -> {
+                    binding.layoutParent.setBackgroundResource(R.color.green_verde_clear)
+                }
+
+                "F" -> {
+                    binding.layoutParent.setBackgroundResource(R.color.color_yelon_clear)
+                }
+
+                "R" -> {
+                    binding.layoutParent.setBackgroundResource(R.color.blue)
+                }
+
+                "NR" -> {
+                    binding.layoutParent.setBackgroundResource(R.color.red)
+                }
+            }
+
+            binding.textIdentificacao.text = item.tag
+            binding.textInfo.text = item.descricao
+
+            //Clique no item
+            binding.layoutParent.setOnClickListener {
+                onclick(item)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LeituraRfidEpcAdapterRfidVh {
+        val binding =
+            ItemRvEpcRfidBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return LeituraRfidEpcAdapterRfidVh(binding)
+    }
+
+    override fun getItemCount() = list.size
+
+    override fun onBindViewHolder(holder: LeituraRfidEpcAdapterRfidVh, position: Int) {
+        holder.bind(list[position])
+    }
+
+    fun updateData(listNf: List<LeituraRfidEpc>) {
+        list.clear()
+        list.addAll(listNf)
+        notifyDataSetChanged()
+    }
+
+}
