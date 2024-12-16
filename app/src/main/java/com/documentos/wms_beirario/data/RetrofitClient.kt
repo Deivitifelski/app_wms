e4811f7e3b6c01211c4678831fd949b440ff096b
deiviti_efisio <deiviti.efisio@paipe.co>
2024-12-16 20:43:29 -0300

    delete enum type conection

--- app/src/main/java/com/documentos/wms_beirario/data/RetrofitClient.kt
+++ app/src/main/java/com/documentos/wms_beirario/data/RetrofitClient.kt
@@ -1,11 +1,13 @@
 package com.documentos.wms_beirario.data
 
+import android.util.Log
 import kotlinx.coroutines.handleCoroutineException
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 class RetrofitClient {
@@ -14,6 +16,7 @@ class RetrofitClient {
     }
 
     private var retrofit: Retrofit? = null
+
     fun getClient(): ServiceApi {
         val httpOk = HttpLoggingInterceptor()
         httpOk.level = HttpLoggingInterceptor.Level.BODY
@@ -25,8 +28,27 @@ class RetrofitClient {
                 .writeTimeout(60, TimeUnit.SECONDS)
                 .callTimeout(60, TimeUnit.SECONDS)
                 .addInterceptor(httpOk)
+                .addInterceptor { chain ->
+                    val request = chain.request()
+                    val host = request.url.host
+
+                    // Lista de hosts a serem bloqueados
+                    val blockedHosts = listOf(
+                        "ssdk-sg.pangle.io",
+                        "tnc16-alisg.isnssdk.com",
+                        "sf16-static.i18n-pglstatp.com"
+                    )
+
+                    if (blockedHosts.contains(host)) {
+                        Log.e("->", "\nRequisição bloqueada para o host: $host\n" )
+                        throw IOException("Requisição bloqueada para o host: $host")
+                    }
+
+                    chain.proceed(request)
+                }
                 .retryOnConnectionFailure(true)
                 .build()
+
             retrofit = Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -37,3 +59,4 @@ class RetrofitClient {
         return retrofit!!.create(ServiceApi::class.java)
     }
 }
+
